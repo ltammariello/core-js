625e44133e8bc840193c08c6564111f803004930
Denis Pushkarev <zloirock@zloirock.ru>
2017-10-27 02:47:35 +0700

    rewrite Gruntfile from LiveScript to JavaScript

--- library/stage/2.js
+++ library/stage/2.js
@@ -1,4 +1,5 @@
-require('../modules/esnext.symbol.async-iterator');
+require('../modules/esnext.array.flat-map');
+require('../modules/esnext.array.flatten');
 require('../modules/esnext.string.trim-left');
 require('../modules/esnext.string.trim-right');
 module.exports = require('./3');
